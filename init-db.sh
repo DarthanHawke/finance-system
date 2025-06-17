d8f74c538c93a6f5a039c861d382176e6eeddb73
Alexey Hawke <drakon9854@gmail.com>
2025-06-17 16:00:54 +0300

    update sso, payment, docker-compose

--- init-db.sh
+++ init-db.sh
@@ -3,7 +3,7 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE payment_db;
-    CREATE DATABASE auth_db;
+    CREATE DATABASE sso_db;
     GRANT ALL PRIVILEGES ON DATABASE payment_db TO postgres;
-    GRANT ALL PRIVILEGES ON DATABASE auth_db TO postgres;
+    GRANT ALL PRIVILEGES ON DATABASE sso_db TO postgres;
 EOSQL
