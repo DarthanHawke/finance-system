273d35eb4e6faa57cb6c3e12d941c2cf46840286
Alexey Hawke <drakon9854@gmail.com>
2025-06-26 18:39:40 +0300

    update billing, fix sso, protos

--- scripts/postgres/init-db.sh
+++ scripts/postgres/init-db.sh
@@ -4,6 +4,8 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE payment_db;
     CREATE DATABASE sso_db;
+    CREATE DATABASE billing_db;
     GRANT ALL PRIVILEGES ON DATABASE payment_db TO postgres;
     GRANT ALL PRIVILEGES ON DATABASE sso_db TO postgres;
+    GRANT ALL PRIVILEGES ON DATABASE billing_db TO postgres;
 EOSQL
